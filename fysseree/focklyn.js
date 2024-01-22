e7d92f3c90faaae1c37db41c3a2d1e15bd26474b
peddyrbeg <85888409+peddyrbeg@users.noreply.github.com>
2024-01-22 11:37:15 +0000

    Add files via upload

---
+++ fysseree/focklyn.js
@@ -0,0 +1,98 @@
+const focklynYnsit = {
+    "braew": {
+        caslys: "caslyssyn/braew.png",
+        sheean: "sheeanyn/braew.mp3"
+    },
+    "çhing": {
+        caslys: "caslyssyn/ching.png",
+        sheean: "sheeanyn/ching.mp3"
+    },
+    "trimshagh": {
+        caslys: "caslyssyn/trimshagh.png",
+        sheean: "sheeanyn/trimshagh.mp3"
+    },
+    "maynrey": {
+        caslys: "caslyssyn/maynrey.png",
+        sheean: "sheeanyn/maynrey.mp3"
+    },
+    "feayr": {
+        caslys: "caslyssyn/feayr.png",
+        sheean: "sheeanyn/feayr.mp3"
+    },
+    "çheh": {
+        caslys: "caslyssyn/cheh.png",
+        sheean: "sheeanyn/cheh.mp3"
+    },
+    "greesit": {
+        caslys: "caslyssyn/greesit.png",
+        sheean: "sheeanyn/greesit.mp3"
+    },
+    "skee": {
+        caslys: "caslyssyn/skee.png",
+        sheean: "sheeanyn/skee.mp3"
+    },
+    "corree": {
+        caslys: "caslyssyn/corree.png",
+        sheean: "sheeanyn/corree.mp3"
+    }
+};
+
+// const focklynYnsit = {
+//     "nane": "caslyssyn/nane.png",
+//     "jees": "caslyssyn/jees.png",
+//     "tree": "caslyssyn/tree.png",
+//     "kiare": "caslyssyn/kiare.png",
+//     "queig": "caslyssyn/queig.png",
+//     "shey": "caslyssyn/shey.png",
+//     "shiaght": "caslyssyn/shiaght.png",
+//     "hoght": "caslyssyn/hoght.png",
+//     "nuy": "caslyssyn/nuy.png",
+//     "jeih": "caslyssyn/jeih.png"
+// };
+
+// const focklynYnsit = {
+//     "Doolish": "caslyssyn/nane.png",
+//     "Laksaa": "caslyssyn/jees.png",
+//     "Rhumsaa": "caslyssyn/tree.png",
+//     "Purt-ny-hInshey": "caslyssyn/kiare.png",
+//     "Purt Çhiarn": "caslyssyn/queig.png",
+//     "Purt le Moirrey": "caslyssyn/shey.png",
+//     "Balley Chashtal": "caslyssyn/shiaght.png"
+// };
+
+// const focklynYnsit = {
+//     "braew": "caslyssyn/braew emshir.png",
+//     "çheh": "caslyssyn/cheh emshir.png",
+//     "feayr": "caslyssyn/feayr emshir.png",
+//     "sterrymagh": "caslyssyn/sterrymagh.png",
+//     "grianagh": "caslyssyn/grianagh.png",
+//     "bodjallagh": "caslyssyn/bodjallagh.png",
+//     "geayagh": "caslyssyn/geayagh.png",
+//     "ceau fliaghey": "caslyssyn/ceau fliaghey.png",
+//     "ceau sniaghtey": "caslyssyn/ceau sniaghtey.png",
+// };
+
+// const focklynYnsit = {
+//     "ooylyn": "caslyssyn/braew emshir.png",
+//     "bagoon": "caslyssyn/cheh emshir.png",
+//     "arran": "caslyssyn/feayr emshir.png",
+//     "caashey": "caslyssyn/sterrymagh.png",
+//     "spollagyn": "caslyssyn/grianagh.png",
+//     "eeast": "caslyssyn/bodjallagh.png",
+//     "noiridyn": "caslyssyn/geayagh.png",
+//     "pronnagyn": "caslyssyn/ceau fliaghey.png",
+//     "lossreeyn": "caslyssyn/ceau sniaghtey.png",
+// };
+
+// const focklynYnsit = {
+//     "gee": "caslyssyn/braew emshir.png",
+//     "cloie": "caslyssyn/cheh emshir.png",
+//     "bluckan coshey": "caslyssyn/feayr emshir.png",
+//     "cloie Xbox": "caslyssyn/sterrymagh.png",
+//     "lhaih": "caslyssyn/grianagh.png",
+//     "markiaght": "caslyssyn/bodjallagh.png",
+//     "roie": "caslyssyn/geayagh.png",
+//     "cadley": "caslyssyn/ceau fliaghey.png",
+//     "snaue": "caslyssyn/ceau sniaghtey.png",
+//     "shooyl": "caslyssyn/ceau sniaghtey.png",
+// };
